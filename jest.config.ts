d693a07fe38e482b0044597a9b9e0431e774badd
Renan Fortunato <renanfortunato09@gmail.com>
2022-03-01 14:26:34 -0300

    chore: add editorconfig file

--- jest.config.ts
+++ jest.config.ts
@@ -4,33 +4,13 @@
  */
 
 export default {
-  // All imported modules in your tests should be mocked automatically
-  // automock: false,
-
-  // Stop running tests after `n` failures
-  // bail: 0,
-
-  // The directory where Jest should store its cached dependency information
-  // cacheDirectory: "/private/var/folders/sv/ksck7gcs57z_bd85d1sjsdrc0000gp/T/jest_dy",
-
-  // Automatically clear mock calls, instances and results before every test
   clearMocks: true,
-
   // Indicates whether the coverage information should be collected while executing the test
   collectCoverage: true,
-
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
-
-  // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
-
-  // Indicates which provider should be used to instrument code for coverage
   coverageProvider: "v8",
 
   // A list of reporter names that Jest uses when writing coverage reports
